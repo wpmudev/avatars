1827ef9211c0a3ef6993137f51ddb61357dce3b1
ignacio <ignacio@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-03-05 09:04:37 +0000

    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/avatars@13086 f53fe46e-8c02-459b-9fdd-93e75771b278

--- avatars.php
+++ avatars.php
@@ -1424,17 +1424,3 @@ class Avatars {
 }
 $ms_avatar = new Avatars();
 
-
-
-/**
- * Show notification if WPMUDEV Update Notifications plugin is not installed
- **/
-if ( !function_exists( 'wdp_un_check' ) ) {
-	add_action( 'admin_notices', 'wdp_un_check', 5 );
-	add_action( 'network_admin_notices', 'wdp_un_check', 5 );
-
-	function wdp_un_check() {
-		if ( !class_exists( 'WPMUDEV_Update_Notifications' ) && current_user_can( 'edit_users' ) )
-			echo '<div class="error fade"><p>' . __('Please install the latest version of <a href="http://premium.wpmudev.org/project/update-notifications/" title="Download Now &raquo;">our free Update Notifications plugin</a> which helps you stay up-to-date with the most stable, secure versions of WPMU DEV themes and plugins. <a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">More information &raquo;</a>', 'wpmudev') . '</p></div>';
-	}
-}
